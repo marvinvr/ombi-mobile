7eb8467e1434fc831c7b41a58fdc872050cf4f52
Marvin von Rappard <marvin@von-rappard.ch>
2020-12-08 21:14:06 +0000

    feat: :cop: auth + api services

--- src/services/api.service.spec.ts
+++ src/services/api.service.spec.ts
@@ -1,13 +1,13 @@
 import { TestBed } from '@angular/core/testing';
 
-import { TokenService } from './token.service';
+import { ApiService } from './api.service';
 
-describe('TokenService', () => {
-  let service: TokenService;
+describe('ApiService', () => {
+  let service: ApiService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(TokenService);
+    service = TestBed.inject(ApiService);
   });
 
   it('should be created', () => {
