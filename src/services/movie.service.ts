275720fe174989036b27acc913061016c82054f4
Jelle Schutter <jelle@schutter.xyz>
2020-12-09 23:18:17 +0100

    feat: :movie: adds movie service (#32)

---
+++ src/services/movie.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { Item } from 'src/models/item';
+import { MovieSearchType } from 'src/models/movie';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class MovieService {
+
+  constructor(
+    private api: ApiService
+  ) { }
+
+  public list(type: MovieSearchType = MovieSearchType.POPULAR): Promise<Array<Item>> {
+    return this.api.getRequest(`/search/Movie/${type}`, {}, {})
+      .toPromise()
+      .then(this.toItems)
+  }
+
+  public search(term: string): Promise<Array<Item>> {
+    return this.api.postRequest('/search/Movie/', {}, {
+      searchTerm: term,
+      languageCode: 'en'
+    })
+    .toPromise()
+    .then(this.toItems)
+  }
+
+  private toItems(results: Array<any>): Array<Item> {
+    return results.map((item) => {
+      return {
+        id: item.theMovieDbId,
+        title: item.originalTitle,
+        posterUrl: `https://image.tmdb.org/t/p/w300${item.posterPath}`,
+        description: item.overview,
+        requested: item.requested
+      }
+    })
+  }
+}
