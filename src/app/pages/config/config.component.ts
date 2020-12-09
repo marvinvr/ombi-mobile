11e509ea2a83a0de346dc6f70e21c2e6dde4becf
Marvin von Rappard <marvin@von-rappard.ch>
2020-12-09 20:53:18 +0000

    feat: :gear: config page

--- src/app/pages/config/config.component.ts
+++ src/app/pages/config/config.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/services/auth.service';
+import { CredentialsService } from 'src/services/credentials.service';
+import { InputType } from '../../../models/input';
 
 @Component({
   selector: 'app-config',
@@ -6,9 +9,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./config.component.scss'],
 })
 export class ConfigComponent implements OnInit {
+  public model = {
+    ombiUrl: '',
+    username: '',
+    password: ''
+  }
 
-  constructor() { }
+  constructor(
+    private credentials: CredentialsService,
+    private auth: AuthService
+  ) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.model.ombiUrl = this.credentials.baseUrl;
+    this.model.username = this.credentials.username;
+    this.model.password = this.credentials.password;
+  }
+
+  public submit() {
+    this.credentials.baseUrl = this.model.ombiUrl;
+    this.credentials.username = this.model.username;
+    this.credentials.password = this.model.password;
+    this.auth.fetchToken()
+      .subscribe( t => this.credentials.token = t?.access_token)
+  }
+
+  public get inputType(): typeof InputType {
+    return InputType; 
+  }
 
 }
