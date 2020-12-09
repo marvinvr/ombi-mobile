13a06bd69f40a1df8ef8e9beb595fd58556889a3
Jelle Schutter <jelle@schutter.xyz>
2020-12-09 23:28:29 +0100

    remove unused import

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
 import { ApiService } from './api.service';
 import { CredentialsService } from './credentials.service';
 
