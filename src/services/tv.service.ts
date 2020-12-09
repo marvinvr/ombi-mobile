1df078cd1adbc2d2c885a96db269684b6c82664a
Marvin von Rappard <marvin@von-rappard.ch>
2020-12-09 21:31:12 +0000

    feat: :tv: creates tv service

---
+++ src/services/tv.service.ts
@@ -0,0 +1,22 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { TvSearchType } from 'src/models/tv';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TvService {
+
+  constructor(
+    private api: ApiService
+  ) { }
+
+  list(type: TvSearchType): Observable<Object> {
+    return this.api.getRequest(`/search/Tv/${type}`, {}, {});
+  }
+
+  search(term: string): Observable<Object> {
+    return this.api.getRequest(`/search/Tv/${term}`, {}, {});
+  }
+}
