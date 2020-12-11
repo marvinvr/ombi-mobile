8542ec3514ff20e10e692630e09fd383275de4b4
Marvin von Rappard <marvin@von-rappard.ch>
2020-12-11 18:59:05 +0000

    fix: :bug: adds toast to requests

--- src/services/requests.service.ts
+++ src/services/requests.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { RequestAction, RequestActionType, RequestAvailability, RequestSort, RequestStatus, RequestType } from 'src/models/requests';
+import { ToastType } from 'src/models/toast';
 import { ApiService } from './api.service';
+import { ToastService } from './toast.service';
 
 @Injectable({
   providedIn: 'root'
@@ -9,6 +11,7 @@ export class RequestsService {
 
   constructor(
     private api: ApiService,
+    private toast: ToastService
   ) { }
 
   public list(
@@ -29,15 +32,16 @@ export class RequestsService {
       return this.api.get(`/Request/${type}/search/${term}`, {}, {});
   }
 
-  public deny(type: RequestActionType, id: number = 0) {
-    this.performAction(RequestAction.DENY, type, id);
+  public deny(type: RequestActionType, id: number = 0): Promise<any> {
+    return this.performAction(RequestAction.DENY, type, id);
   }
 
-  public approve(type: RequestActionType, id: number = 0) {
-    this.performAction(RequestAction.APPROVE, type, id);
+  public approve(type: RequestActionType, id: number = 0): Promise<any> {
+    return this.performAction(RequestAction.APPROVE, type, id);
   }
 
   private performAction(action: RequestAction, type: RequestActionType, id: number = 0) {
-    return this.api.put(`/Request/${type}/${action}`, {}, {id: id});
+    return this.api.put(`/Request/${type}/${action}`, {}, {id: id})
+            .then(() => this.toast.show(ToastType.SUCCESS, `Successfully ${action == RequestAction.APPROVE ? 'approved' : 'denied'} request`));
   }
 }
