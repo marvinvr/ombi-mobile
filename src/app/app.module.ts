cd805a747782eed4dfe382565bb15a77f6a68101
Marvin von Rappard <marvin@von-rappard.ch>
2023-04-11 13:38:46 +0200

    fix: version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,4 +1,4 @@
-import { NgModule } from '@angular/core';
+import { NgModule, isDevMode } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
 
@@ -49,7 +49,13 @@ import { environment } from '../environments/environment';
         IonicModule.forRoot(),
         AppRoutingModule,
         HttpClientModule,
-        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production })
+        ServiceWorkerModule.register('ngsw-worker.js', { enabled: environment.production }),
+        ServiceWorkerModule.register('ngsw-worker.js', {
+          enabled: !isDevMode(),
+          // Register the ServiceWorker as soon as the application is stable
+          // or after 30 seconds (whichever comes first).
+          registrationStrategy: 'registerWhenStable:30000'
+        })
     ],
     providers: [
         { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
